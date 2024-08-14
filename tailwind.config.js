37f7c95ca792b872e461e66e5e5189c4e21acf9c
zerialDott <hellovitaralia@gmail.com>
2024-08-14 03:04:58 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",      // Incluye el archivo HTML en la raíz
+    "./src/**/*.{html,js}"  // Incluye todos los archivos HTML y JS en la carpeta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
